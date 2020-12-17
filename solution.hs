fbadb2096980eed0d5b125ec34112585972d50b5
Kush Baheti <kushbaheti@gmail.com>
2020-12-18 07:25:39 +0800

    [Feature] Implemented optimal solution

--- solution.hs
+++ solution.hs
@@ -1,4 +1,9 @@
+import Data.List
+import Data.Ord
+
 import Kodable
+
+shortest list = minimumBy (comparing length) list -- chaneg names
 
 removeBonus :: [String] -> Int -> Int -> [String]
 removeBonus map x y = take x map ++ [modifiedRow] ++ drop (x + 1) map
@@ -7,35 +12,58 @@ removeBonus map x y = take x map ++ [modifiedRow] ++ drop (x + 1) map
                           modifiedRow = take y row ++ "-" ++ drop (y + 1) row
 
 goRight :: [String] -> Int -> Int -> Int -> ([String], Int, Int, Int)
-goRight map x y bonusCount
-    | y + 2 >= ((length $ head map) - 1) || (map !! x) !! (y + 2) == '*' = (map, x, y, bonusCount) 
-    | (map !! x) !! (y + 2) == 'b' = goRight (removeBonus map x y) x (y + 2) (bonusCount + 1)
-    | (map !! x) !! (y + 2) `elem` ['p', 'o', 'y', 't'] = (map, x, (y + 2), bonusCount)
-    | (map !! x) !! (y + 2) == '-' = goRight map x (y + 2) bonusCount
+goRight map x y bonus
+    | y + 2 >= ((length $ head map) - 1) || (map !! x) !! (y + 2) == '*' = (map, x, y, bonus) 
+    | (map !! x) !! (y + 2) == 'b' = goRight (removeBonus map x (y+2)) x (y + 2) (bonus + 1)
+    | (map !! x) !! (y + 2) `elem` ['p', 'o', 'y', 't'] = (map, x, (y + 2), bonus)
+    -- | (map !! x) !! (y + 2) == '-' = goRight map x (y + 2) bonus
+    | otherwise = goRight map x (y + 2) bonus
 
 goLeft :: [String] -> Int -> Int -> Int -> ([String], Int, Int, Int)
-goLeft map x y bonusCount
-    | y - 2 < 0 || (map !! x) !! (y - 2) == '*' = (map, x, y, bonusCount) 
-    | (map !! x) !! (y - 2) == 'b' = goLeft (removeBonus map x y) x (y - 2) (bonusCount + 1)
-    | (map !! x) !! (y - 2) `elem` ['p', 'o', 'y', 't'] = (map, x, (y - 2), bonusCount)
-    | (map !! x) !! (y - 2) == '-' = goLeft map x (y - 2) bonusCount
+goLeft map x y bonus
+    | y - 2 < 0 || (map !! x) !! (y - 2) == '*' = (map, x, y, bonus) 
+    | (map !! x) !! (y - 2) == 'b' = goLeft (removeBonus map x (y-2)) x (y - 2) (bonus + 1)
+    | (map !! x) !! (y - 2) `elem` ['p', 'o', 'y', 't'] = (map, x, (y - 2), bonus)
+    -- | (map !! x) !! (y - 2) == '-' = goLeft map x (y - 2) bonus
+    | otherwise = goLeft map x (y - 2) bonus
 
 goUp :: [String] -> Int -> Int -> Int -> ([String], Int, Int, Int)
-goUp map x y bonusCount
-    | x - 1 < 0 || (map !! (x - 1)) !! y == '*' = (map, x, y, bonusCount) 
-    | (map !! (x - 1)) !! y == 'b' = goUp (removeBonus map x y) (x - 1) y (bonusCount + 1)
-    | (map !! (x - 1)) !! y `elem` ['p', 'o', 'y', 't'] = (map, (x - 1), y, bonusCount)
-    | (map !! (x - 1)) !! y == '-' = goUp map (x - 1) y bonusCount
+goUp map x y bonus
+    | x - 1 < 0 || (map !! (x - 1)) !! y == '*' = (map, x, y, bonus) 
+    | (map !! (x - 1)) !! y == 'b' = goUp (removeBonus map (x-1) y) (x - 1) y (bonus + 1)
+    | (map !! (x - 1)) !! y `elem` ['p', 'o', 'y', 't'] = (map, (x - 1), y, bonus)
+    -- | (map !! (x - 1)) !! y == '-' = goUp map (x - 1) y bonus
+    | otherwise = goUp map (x-1) y bonus
 
 goDown :: [String] -> Int -> Int -> Int -> ([String], Int, Int, Int)
-goDown map x y bonusCount
-    | x + 1 > ((length map) - 1) || (map !! (x + 1)) !! y == '*' = (map, x, y, bonusCount) 
-    | (map !! (x + 1)) !! y == 'b' = goDown (removeBonus map x y) (x + 1) y (bonusCount + 1)
-    | (map !! (x + 1)) !! y `elem` ['p', 'o', 'y', 't'] = (map, (x + 1), y, bonusCount)
-    | (map !! (x + 1)) !! y == '-' = goDown map (x + 1) y bonusCount
+goDown map x y bonus
+    | x + 1 > ((length map) - 1) || (map !! (x + 1)) !! y == '*' = (map, x, y, bonus) 
+    | (map !! (x + 1)) !! y == 'b' = goDown (removeBonus map (x+1) y) (x + 1) y (bonus + 1)
+    | (map !! (x + 1)) !! y `elem` ['p', 'o', 'y', 't'] = (map, (x + 1), y, bonus)
+    -- | (map !! (x + 1)) !! y == '-' = goDown map (x + 1) y bonus
+    | otherwise = goDown map (x+1) y bonus
 
 -- -- t @ - p o y b * {IoB}
--- optimalSolutionUtil :: [String] -> [String]
--- optimalSolutionUtil map x y currentDirection visited allSolutions currentSolution bonusCount
---     | cell == 't' && bonusCount == 3 = allSolutions ++ [currentSolution] 
---     | isColor cell || isAt cell = allSolutions ++ [rightPath] ++ [leftPath] ++ [upPath] ++ [downPath]
+optimalSolutionUtil :: [String] -> Int -> Int -> [(Int, Int, Int)] -> [String] -> Int -> [[String]]
+optimalSolutionUtil map x y visited solution bonus
+    | (cell == 't' && bonus /= 3) = []
+    | cell == 't' && bonus == 3 = [solution]
+    | isColor cell = (rightPath (condString cell "Right")) ++ (leftPath (condString cell "Left")) ++ (upPath (condString cell "Up")) ++ (downPath (condString cell "Down"))
+    | otherwise = (rightPath "Right") ++ (leftPath "Left") ++ (upPath "Up") ++ (downPath "Down")
+        where
+            cell = (map !! x) !! y
+            isColor c = c `elem` ['p', 'o', 'y']
+            -- isDash c = c == '-'
+            -- isAt c = c == '@'
+            condString cell s = "Cond{" ++ [cell] ++ "}{" ++ s ++ "}"
+            (mapR, xR, yR, bonusR) = goRight map x y bonus
+            rightPath s = if ((x == xR && y == yR) || (xR, yR, bonusR) `elem` visited) then [] else optimalSolutionUtil mapR xR yR (visited ++ [(xR, yR, bonusR)]) (solution ++ [s]) bonusR 
+            (mapL, xL, yL, bonusL) = goLeft map x y bonus
+            leftPath s  = if ((x == xL && y == yL) || (xL, yL, bonusL) `elem` visited) then [] else optimalSolutionUtil mapL xL yL (visited ++ [(xL, yL, bonusL)]) (solution ++ [s]) bonusL
+            (mapU, xU, yU, bonusU) = goUp map x y bonus
+            upPath s    = if ((x == xU && y == yU) || (xU, yU, bonusU) `elem` visited) then [] else optimalSolutionUtil mapU xU yU (visited ++ [(xU, yU, bonusU)]) (solution ++ [s]) bonusU 
+            (mapD, xD, yD, bonusD) = goDown map x y bonus
+            downPath s  = if ((x == xD && y == yD) || (xD, yD, bonusD) `elem` visited) then [] else optimalSolutionUtil mapD xD yD (visited ++ [(xD, yD, bonusD)]) (solution ++ [s]) bonusD 
+
+
+
